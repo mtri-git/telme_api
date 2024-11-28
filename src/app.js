4e1095206a32412dd2fc542c4da7f11a19593695
mtri-git <vmtri20@gmail.com>
2024-11-28 12:09:56 +0700

    chore: update local cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const registerRoutes = require("./routes/router");
 const cors = require("@fastify/cors");
 
 fastify.register(cors, {
-  origin: ["http://localhost:3000", "https://example.com"], // Các domain được phép truy cập
+  origin: ["http://localhost:3005", "https://example.com"], // Các domain được phép truy cập
   methods: ["GET", "POST", "PUT", "DELETE"], // Các method HTTP được phép
   allowedHeaders: ["Content-Type", "Authorization"], // Các header được phép
   credentials: true, // Hỗ trợ gửi cookie (cross-site)
